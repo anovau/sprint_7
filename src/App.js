2929343fe922a677ca1378f66402f475d91835c6
anovau <novau_678@hotmail.com>
2022-03-24 12:19:47 +0100

    Form with 3 Checkbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  // HOOKS
+  const [budget, setBudget] = useState(0);
+
+  //METHODS
+  const addToBudget = (event) => {
+    if (event.target.checked) {
+      setBudget(budget + parseInt(event.target.value));
+    } else {
+      setBudget(budget - parseInt(event.target.value));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <p>¿Qué quieres hacer?</p>
+      <label>
+        <input
+          type="checkbox"
+          id=""
+          name=""
+          value="500"
+          onChange={addToBudget}
+        />
+        Una página web (500€)
+      </label>
+      <br />
+      <label>
+        <input 
+          type="checkbox" 
+          name="" 
+          value="300" 
+          onChange={addToBudget} 
+        />
+        Una consultoria SEO (300€)
+      </label>
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          id=""
+          name=""
+          value="200"
+          onChange={addToBudget}
+        />
+        Una campanya de Google Ads (200€)
+      </label>
+      <p>Preu: {budget}€</p>
+    </form>
   );
 }
 
